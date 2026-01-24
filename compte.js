ab3b448ee35ce2944c216c5e15f1eb0e50985b0d
2025qianfan-png <2025qianfan@gmail.com>
2026-01-24 22:40:58 +0100

    Add files via upload

---
+++ compte.js
@@ -0,0 +1,6 @@
+const students = [
+  
+  { name: "peng.gao", password: "peng2026",type:"m",timer:"2027-01-01" },
+  { name: "admin", password: "mv2026" ,type:"t",timer:"2026-01-24"},
+  { name: "shengyang.qi", password: "shengyang2026",type:"r" ,timer:"2026-01-01"}
+];
