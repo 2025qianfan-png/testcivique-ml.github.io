f9cc6c67f08b2018e1e304b53935cf898011ffda
2025qianfan-png <2025qianfan@gmail.com>
2026-01-29 09:36:12 +0100

    更新 compte.js

--- compte.js
+++ compte.js
@@ -5,8 +5,9 @@ const students = [
   { name: "shengyang.qi", password: "shengyang2026",type:"r" ,timer:"2026-05-01"},
   { name: "dan.wang", password: "wang2026" ,type:"n",timer:"2999-01-01"},
    { name: "yue.lyu", password: "lyu2026",type:"m",timer:"2999-01-01" },
-{ name: "quoala.quollant", password: "quollant2026",type:"r",timer:"2999-01-01" }
-
+{ name: "quoala.quollant", password: "quollant2026",type:"r",timer:"2999-01-01" },
+  { name: "sun", password: "sun2026" ,type:"n",timer:"2026-03-01"}
+   
 ];
 
 
